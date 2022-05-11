7e5916b22ea0890c77e9b29038ffeddeed9b9e55
ifcassianasl <cassianasilveira64@gmail.com>
2022-05-11 15:16:26 -0300

    add template

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 import ffmpeg_streaming
 from ffmpeg_streaming import Formats
 
@@ -8,11 +9,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def streaming():
-  video = ffmpeg_streaming.input('rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mp4')
+  return render_template('streaming.html')
+
+@app.route('/video')
+def video_server():
+  video = ffmpeg_streaming.input('pexels_video.mp4')
 
   hls = video.hls(Formats.h264())
   hls.auto_generate_representations()
-  return hls.output('/var/media/hls.m3u8', monitor=monitor)
+  hls.save_master_playlist('/var/media/hls.m3u8')
+
+  return hls.output('/var/media/hls.m3u8')
 
 def monitor(ffmpeg, duration, time_, time_left, process):
   per = round(time_ / duration * 100)
